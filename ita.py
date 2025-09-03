6ba805214bb996993ab1dd4790fc5323fb117b0b
Kfir Carmely <73598682+Kfir2k17@users.noreply.github.com>
2025-09-03 20:24:54 +0300

    final yes

--- ita.py
+++ ita.py
@@ -1,1 +1,24 @@
+import os, sys
+from PIL import Image
+
+txt = open("text.txt", "w")
+
 COLORS = "MNHQ$OC?7>!:-;. "
+
+im1 = Image.open("image.jpg")
+im1 = im1.convert("L")
+
+width, height = im1.size
+
+im2 = Image.new("RGB", (width, height))
+
+for y in range(height):
+    for x in range(width):
+        p = im1.getpixel((x, y))
+
+        txt.write(COLORS[int(p // 16)])
+
+        txt.write("  ")
+    txt.write("\n")
+
+txt.close()
