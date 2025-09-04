dea0ec87e8eff74e9980505466eebdf136548374
Kfir Carmely <73598682+Kfir2k17@users.noreply.github.com>
2025-09-04 08:51:53 +0300

    more organized order

--- ita.py
+++ ita.py
@@ -6,19 +6,33 @@ txt = open("text.txt", "w")
 COLORS = "MNHQ$OC?7>!:-;. "
 
 im1 = Image.open("image.jpg")
-im1 = im1.convert("L")
+im2 = Image.open("bb.png")
 
-width, height = im1.size
+def convert_image_to_ascii(image):
+    image = image.convert("L")
+    width, height = image.size
 
-im2 = Image.new("RGB", (width, height))
+    for y in range(height):
+        for x in range(width):
+            p = image.getpixel((x, y))
 
-for y in range(height):
-    for x in range(width):
-        p = im1.getpixel((x, y))
+            txt.write(COLORS[int(p // 16)])
 
-        txt.write(COLORS[int(p // 16)])
+            txt.write("  ")
+        txt.write("\n")
 
-        txt.write("  ")
-    txt.write("\n")
+    txt.close()
 
-txt.close()
+def convert_image_into_ascii_image(image):
+    image = image.convert("L")
+    width, height = image.size
+
+
+
+    for y in range(height):
+        for x in range(width):
+            p = image.getpixel((x, y))
+
+
+
+convert_image_to_ascii(im1)
